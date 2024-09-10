4d84c1d99b5642b04cf93572eb7e85f0e1ca35e3
Ubuntu <ubuntu@ip-172-31-29-246.us-west-2.compute.internal>
2024-09-10 06:46:33 +0000

    Add Terraform configuration and GitHub Actions workflow

---
+++ terraform/main.tf
@@ -0,0 +1,92 @@
+provider "aws" {
+  region = "us-west-2"  # Updated to your region
+}
+
+# VPC Setup
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "subnet" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-west-2a"  # Updated to us-west-2 availability zone
+}
+
+# Security Group for ECS
+resource "aws_security_group" "ecs_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# ECS Cluster
+resource "aws_ecs_cluster" "main" {
+  name = "medusa-cluster"
+}
+
+# IAM Role for ECS Fargate Task
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.id
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# Fargate Task Definition
+resource "aws_ecs_task_definition" "medusa" {
+  family                   = "medusa-task"
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
+
+  container_definitions = jsonencode([{
+    name  = "medusa"
+    image = "medusajs/medusa:v1"
+    essential = true
+    portMappings = [{
+      containerPort = 80
+      hostPort      = 80
+    }]
+  }])
+}
+
+# ECS Service
+resource "aws_ecs_service" "medusa_service" {
+  name            = "medusa-service-unique-name"  
+  cluster         = aws_ecs_cluster.main.id
+  task_definition = aws_ecs_task_definition.medusa.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets         = ["subnet-0780cded51974dbe1"]
+    security_groups = ["sg-062d693d35ec27f18"]
+  }
+}
